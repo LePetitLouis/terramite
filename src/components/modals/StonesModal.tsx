4d46f296179f30ffe75da7a088f423046f213971
Louis Poupat <louispoupat@sfr.fr>
2022-12-05 15:35:35 +0100

    [fix] country names

--- src/components/modals/StonesModal.tsx
+++ src/components/modals/StonesModal.tsx
@@ -1,18 +1,22 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useMemo, useState } from "react";
 
 import { DB, IStones } from "../../datas/db";
 import { StoneCard } from "../cards/StoneCard";
 
 import mainContext from "../../contexts/modalContext";
 
-import { BaseModal } from "./BaseModal"
+import { BaseModal } from "./BaseModal";
 
 export const StonesModal = ({ country }: { country: string }) => {
   const [stones, setStones] = useState<IStones[]>([]);
+  const [countryName, setCountryName] = useState<string>("");
+
+  const regionNames = useMemo(() => new Intl.DisplayNames(["fr"], { type: "region" }), [])
 
   const { updateShowBannerModal } = useContext(mainContext);
 
   useEffect(() => {
+    setCountryName(regionNames.of(country) as string);
     const getStoneByCountry = DB.countries[country];
     if (!getStoneByCountry) return setStones([]);
 
@@ -26,9 +30,23 @@ export const StonesModal = ({ country }: { country: string }) => {
       }
     );
     setStones(stoneAvailable);
-  }, [country]);
+  }, [country, regionNames]);
 
   return (
-    <BaseModal showModal={true} position="middle" title={country} children={stones.map(stone => { return (<StoneCard name={stone.name} picture={stone.image} description={stone.description} />) })} closeModal={() => updateShowBannerModal(false)} />
-  )
-}
+    <BaseModal
+      showModal={true}
+      position="middle"
+      title={countryName}
+      children={stones.map((stone) => {
+        return (
+          <StoneCard
+            name={stone.name}
+            picture={stone.image}
+            description={stone.description}
+          />
+        );
+      })}
+      closeModal={() => updateShowBannerModal(false)}
+    />
+  );
+};
